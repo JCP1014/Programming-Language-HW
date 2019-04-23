e0ee0aaddf922e8e62a3ae18de43071204116a01
JCP1014 <believe861014@gmail.com>
2019-04-24 06:14:10 +0800

    Parse all pages

--- HW2/question_1.py
+++ HW2/question_1.py
@@ -3,6 +3,7 @@ import re
 from collections import Counter
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
 name_cut = input("Input Author: ").split()
 author = name_cut[0]
@@ -14,19 +15,33 @@ content = urllib.request.urlopen(url)
 html_str = content.read().decode('utf-8')
 pattern = "originally announced[\s\S]*?</p>"
 result = re.findall(pattern, html_str)
-pattern = "\d+"
-all_year = []
-for r in result:
-	year = int(re.findall(pattern,r)[0])
-	all_year.append(year)
+if(len(result)>0):
+	pattern = "title is-clearfix[\s\S]*?results"
+	result = re.findall(pattern,html_str)
+	temp = result[0].split("of")[1]
+	total = float(re.findall("\d+",temp)[0])
+	pages = math.ceil(total/50)
+	start = 0
+	all_year = []
+	for i in range(pages):
+		url = "https://arxiv.org/search/?query=" + author + "&searchtype=author&start=" + str(start)
+		content = urllib.request.urlopen(url)
+		html_str = content.read().decode('utf-8')
+		pattern = "originally announced[\s\S]*?</p>"
+		result = re.findall(pattern, html_str)
+		pattern = "\d+"
+		for r in result:
+			year = int(re.findall(pattern,r)[0])
+			all_year.append(year)
+		start += 50
 
-# Plot bar graph
-if(len(all_year)>0):
-	label, value = zip(*Counter(all_year).items())
-	x = np.arange(min(all_year),max(all_year)+1)
-	plt.yticks(np.arange(min(value),max(value)+1))
-	plt.bar(x,value)
-	plt.show()
+	# Plot bar graph
+	if(len(all_year)>0):
+		label, value = zip(*Counter(all_year).items())
+		x = np.arange(min(all_year),max(all_year)+1)
+		plt.yticks(np.arange(min(value),max(value)+1))
+		plt.bar(x,value)
+		plt.show()
 else:
 	print("Sorry, your query for author produced no results.")
 
--- HW2/question_2.py
+++ HW2/question_2.py
@@ -4,6 +4,7 @@ from collections import Counter
 import matplotlib.pyplot as plt
 import numpy as np
 from operator import methodcaller
+import math
 
 name_cut = input("Input Author: ").split()
 author = name_cut[0]
@@ -17,13 +18,27 @@ pattern = "Authors:[\s\S]*?</p>"
 result = re.findall(pattern, html_str)
 
 if(len(result)>0):
+	pattern = "title is-clearfix[\s\S]*?results"
+	result = re.findall(pattern,html_str)
+	temp = result[0].split("of")[1]
+	total = float(re.findall("\d+",temp)[0])
+	pages = math.ceil(total/50)
+	start = 0
 	co_author = []
-	pattern = "a href=\"[\s\S]*?\">[\s\S]*?</a>"
-	for r in result:
-		name_result = re.findall(pattern,r)
-		for n in name_result:
-			name = n.split("\">")[1].split("</a>")[0].strip()
-			co_author.append(name)
+	for i in range(pages):
+		url = "https://arxiv.org/search/?query=" + author + "&searchtype=author&start=" + str(start)
+		content = urllib.request.urlopen(url)
+		html_str = content.read().decode('utf-8')
+		pattern = "Authors:[\s\S]*?</p>"
+		result = re.findall(pattern, html_str)
+
+		pattern = "a href=\"[\s\S]*?\">[\s\S]*?</a>"
+		for r in result:
+			name_result = re.findall(pattern,r)
+			for n in name_result:
+				name = n.split("\">")[1].split("</a>")[0].strip()
+				co_author.append(name)
+		start += 50
 
 	pattern = "[\s\S]*"
 	for n in name_cut:
@@ -42,5 +57,6 @@ if(len(result)>0):
 			print("["+co_name[i]+"]: ",times[i]," times")
 	else:
 		print("No co-author")
+
 else:
 	print("Sorry, your query for author produced no results.")
