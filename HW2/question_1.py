b08fae51140f0bf6b17248b1bd24912b9165452c
JCP1014 <believe861014@gmail.com>
2019-05-02 03:48:06 +0800

    Add comments

--- HW2/question_1.py
+++ HW2/question_1.py
@@ -5,57 +5,67 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
-input_name = input("Input Author: ")
-name_cut = input_name.split()
+input_name = input("Input Author: ")	# Ask user to input author's name
+
+# Split the input name into several words and add "+" between them
+name_cut = input_name.split()	
 author = name_cut[0]
 for i in range(1, len(name_cut)):
 	author += "+"+name_cut[i]
 
-url = "https://arxiv.org/search/?query=" + author + "&searchtype=author"
-content = urllib.request.urlopen(url)
-html_str = content.read().decode('utf-8')
+url = "https://arxiv.org/search/?query=" + author + "&searchtype=author"	# Compose the string of URL
+content = urllib.request.urlopen(url)	# Access the website of search result of the author's name'
+html_str = content.read().decode('utf-8')	# Read HTML of the website and decode them in UTF8
+
 pattern = "originally announced[\s\S]*?</p>"
 result = re.findall(pattern, html_str)
-if(len(result)>0):
+
+if(len(result)>0):	# Ensure there are some results in the website
+	# Get the total number of search results to know how many pages we should parse 
 	pattern = "title is-clearfix[\s\S]*?results"
 	result = re.findall(pattern,html_str)
 	temp = result[0].split("of")[1]
-	total = float(re.findall("\d+",temp)[0])
-	pages = math.ceil(total/50)
-	start = 0
-	all_year = []
+	total = float(re.findall("\d+",temp)[0])	# total number of search results
+	pages = math.ceil(total/50)	# Compute pages
+	start = 0	# page1
+	all_year = []	# list for storing years when those paper was announced
+
 	for i in range(pages):
-		url = "https://arxiv.org/search/?query=" + author + "&searchtype=author&start=" + str(start)
+		url = "https://arxiv.org/search/?query=" + author + "&searchtype=author&start=" + str(start)	# Compose the URL of the page
 		content = urllib.request.urlopen(url)
 		html_str = content.read().decode('utf-8')
-		pattern = "Authors:[\s\S]*?originally announced[\s\S]*?</p>"
-		#pattern = "originally announced[\s\S]*?</p>"
+		pattern = "Authors:[\s\S]*?originally announced[\s\S]*?</p>"	# Find the part of authors' names in every search result
 		result = re.findall(pattern, html_str)
 		check = False
+
 		for r in result:
-			pattern = "a href=\"[\s\S]*?\">[\s\S]*?</a>"
+			# Get every author's name of each paper and check the input author is one of them
+			pattern = "a href=\"[\s\S]*?\">[\s\S]*?</a>"	
 			name_result = re.findall(pattern,r)
 			for n in name_result:
-				name = n.split("\">")[1].split("</a>")[0].strip()
-				if(name.lower()==input_name.lower()):
-					check = True
+				name = n.split("\">")[1].split("</a>")[0].strip()	# Get the name
+				if(name.lower()==input_name.lower()):	 
+					check = True	# Check whether the input author is really in the results
+
+			# If the input author is one of authors of the paper, then get the year when it is announced
 			if(check==True):
 				pattern = "originally announced[\s\S]*?</p>"
 				announce = re.findall(pattern,r)
-				pattern = "\d+"
+				pattern = "\d+"	# Get the number pattern
 				for a in announce:
 					year = int(re.findall(pattern,a)[0])
-					all_year.append(year)
-			check = False	
-		start += 50
+					all_year.append(year)	# Append the year into list
+			check = False	# Reset the boolean
+
+		start += 50	# Turn to next page
 		
 	# Plot bar graph
 	if(len(all_year)>0):
-		label, value = zip(*Counter(all_year).items())
-		plt.yticks(np.arange(min(value),max(value)+1))
+		label, value = zip(*Counter(all_year).items())	# Count years and then pack years and counts into tuples
+		plt.yticks(np.arange(min(value),max(value)+1))	# Set graduation of y-axis
 		plt.bar(label,value)
 		plt.show()
 
-else:
+else:	# There is no result of searching the input author's name
 	print("Sorry, your query for author produced no results.")
 
--- HW2/question_2.py
+++ HW2/question_2.py
@@ -6,58 +6,72 @@ import numpy as np
 from operator import methodcaller
 import math
 
-input_name = input("Input Author: ")
+input_name = input("Input Author: ")	# Ask user to input author's name
+
+# Split the input name into several words and add "+" between them
 name_cut = input_name.split()
 author = name_cut[0]
 for i in range(1, len(name_cut)):
 	author += "+"+name_cut[i]
 
-url = "https://arxiv.org/search/?query=" + author + "&searchtype=author"
-content = urllib.request.urlopen(url)
-html_str = content.read().decode('utf-8')
-pattern = "Authors:[\s\S]*?</p>"
+url = "https://arxiv.org/search/?query=" + author + "&searchtype=author"	# Compose the string of URL
+content = urllib.request.urlopen(url)	# Access the website of search result of the author's name
+html_str = content.read().decode('utf-8')	# Read HTML of the website and decode them in UTF-8
+
+# Find parts of authors' names in search results  
+pattern = "Authors:[\s\S]*?</p>"	
 result = re.findall(pattern, html_str)
 
-if(len(result)>0):
+if(len(result)>0):	# Ensure there are some search results in the website
+	# Get the total number of search results to know how many pages we should parse
 	pattern = "title is-clearfix[\s\S]*?results"
 	result = re.findall(pattern,html_str)
 	temp = result[0].split("of")[1]
-	total = float(re.findall("\d+",temp)[0])
-	pages = math.ceil(total/50)
-	start = 0
-	co_author = []
+	total = float(re.findall("\d+",temp)[0])	# total number of search results
+	pages = math.ceil(total/50)	# Compute pages
+	start = 0	# page1
+	co_author = []	# list for storing co-authors
+
 	for i in range(pages):
-		url = "https://arxiv.org/search/?query=" + author + "&searchtype=author&start=" + str(start)
+		url = "https://arxiv.org/search/?query=" + author + "&searchtype=author&start=" + str(start)	# Composr the URL of the page
 		content = urllib.request.urlopen(url)
 		html_str = content.read().decode('utf-8')
-		pattern = "Authors:[\s\S]*?</p>"
+		pattern = "Authors:[\s\S]*?</p>"	# Find the part of author's name in every search result
 		result = re.findall(pattern, html_str)
 		check = False
-		temp_list = []
+		temp_list = []	# temporary list for storing authors of every paper
+
 		pattern = "a href=\"[\s\S]*?\">[\s\S]*?</a>"
 		for r in result:
+			# Get every author's name in each paper and check whether the input author is one of them
 			name_result = re.findall(pattern,r)
 			for n in name_result:
-				name = n.split("\">")[1].split("</a>")[0].strip()
-				temp_list.append(name)
+				name = n.split("\">")[1].split("</a>")[0].strip()	# Get name
+				temp_list.append(name)	# Append into the temporary list of co-authors
 				if(name.lower()==input_name.lower()):
-					check = True
+					check = True	# Check whether the input author's name is really in the result
+			# If the input author is actually one of the authors of the paper, then append all authors in temporary list into the list of co-authors; if not, abort those names
 			if(check==True):
 				for t in temp_list:
 					co_author.append(t)
-			check = False
-			temp_list = []
-		start += 50
-	
+
+			check = False	# Reset the boolean
+			temp_list = []	# Clear the temporary list for next iteration
+
+		start += 50	# Turn to next page
+	 
+	# Remove those names which are identical to input from co-author list, and the comparison is case-insensitive
 	co_author = [x for x in co_author if x.lower()!=input_name.lower()]
+
+	# Sort the list according to the alphabet
 	co_list = sorted(co_author,key=methodcaller('casefold'))
 	
-	if(len(co_author)>0):
-		co_name, times = zip(*Counter(co_list).items())
-		for i in range(len(co_name)):
-			print("["+co_name[i]+"]: ",times[i]," times")
-	else:
+	if(len(co_author)>0):	# If there are some co-authors in the final list
+		co_name, times = zip(*Counter(co_list).items())	# Count times of each co-author, and then pack names and times into tuples
+		for i in range(len(co_name)):	
+			print("["+co_name[i]+"]:",times[i],"times")	# Print each co-author's name and the times that it appears
+	else:	# Didn't find any co-author
 		print("No co-author")
 
-else:
+else:	# There is no search result on the website
 	print("Sorry, your query for author produced no results.")
